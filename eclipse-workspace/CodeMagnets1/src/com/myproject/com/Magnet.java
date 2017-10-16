828b3ff28943cd76862766e631dc1a196b84a82e
Jesxter <32840048+grantmcg@users.noreply.github.com>
2017-10-16 14:06:14 +0100

    Internal commentary 75% done

--- eclipse-workspace/CodeMagnets1/src/com/myproject/com/Magnet.java
+++ eclipse-workspace/CodeMagnets1/src/com/myproject/com/Magnet.java
@@ -1,20 +1,22 @@
 package com.myproject.com;
 
 public class Magnet {
-
+//Was given scrambled code, task was to rearrang code into a working progam
 	public static void main(String[] args) {
+		//Declaring variables
 		int x = 3;
+		//Performs loop while variable x holds a value greater than 0
 		while (x > 0) {
 			
 			
 			
 		
 			
-			
+			//Statement triggers if variable x is greater than 2, sends a to display
 			if (x > 2) {
 				System.out.print("a");
 			}
-			
+			//Subtracts 1 from var
 			x = x - 1;
 			System.out.print("-");
 		
